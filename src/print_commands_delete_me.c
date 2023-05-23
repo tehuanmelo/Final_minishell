e0dfaaad482b3699744775fd1b4c2dd53c99109d
Tehuan De Melo <tde-melo@lab2r1s1.42abudhabi.ae>
2023-05-23 18:31:08 +0400

    fix redirections within quotes

--- src/print_commands_delete_me.c
+++ src/print_commands_delete_me.c
@@ -10,25 +10,25 @@
 // /*                                                                            */
 // /* ************************************************************************** */
 
-// #include "../inc/minishell.h"
+#include "../inc/minishell.h"
 
-// void print_comands()
-// {
-//     t_cmd *head;
+void print_comands()
+{
+    t_cmd *head;
 
-//     head = data.cmd_lst;
-//     int i = 0;
-//     while (head)
-//     {
-//         printf("command %d\n", i++);
-//         int i = 0;
-//         // printf("arg %d = %s\n", i++, *(head->args));
-//         while (*(head->args))
-//         {
-//             printf("arg %d = %s\n", i++, *(head->args));
-//             head->args++;
-//         }
-//         printf("\n");
-//         head = head->next;
-//     }
-// }
+    head = data.cmd_lst;
+    int i = 0;
+    while (head)
+    {
+        printf("command %d\n", i++);
+        int i = 0;
+        // printf("arg %d = %s\n", i++, *(head->args));
+        while (*(head->args))
+        {
+            printf("arg %d = %s\n", i++, *(head->args));
+            head->args++;
+        }
+        printf("\n");
+        head = head->next;
+    }
+}
--- src/print_tokens_delete_me.c
+++ src/print_tokens_delete_me.c
@@ -1,76 +1,76 @@
 
-// // Do not forget to delete this file before submit the project
+// Do not forget to delete this file before submit the project
 
-// #include "../inc/minishell.h"
+#include "../inc/minishell.h"
 
-// void	print_tokens(t_elem *list)
-// {
-// 	printf("%19s%10s%19s%14s\n", "CONTENT", "LEN", "TYPE", "STATE");
-// 	printf("%*s-----------------------------------------------------\n", 10,
-// 			" ");
-// 	while (list)
-// 	{
-// 		int padding = 20 - list->len;
-// 		printf("%*s::%s::", padding, " ", list->content);
-// 		printf("%9d", list->len);
-// 		switch (list->type)
-// 		{
-// 		case WORD:
-// 			printf("%20s", "WORD");
-// 			break ;
-// 		case REDIR_OUT:
-// 			printf("%20s", "REDIR_OUT");
-// 			break ;
-// 		case REDIR_IN:
-// 			printf("%20s", "REDIR_IN");
-// 			break ;
-// 		case DREDIR_OUT:
-// 			printf("%20s", "DREDIR_OUT");
-// 			break ;
-// 		case PIPE:
-// 			printf("%20s", "PIPE");
-// 			break ;
-// 		case HERE_DOC:
-// 			printf("%20s", "HERE_DOC");
-// 			break ;
-// 		case ENV:
-// 			printf("%20s", "ENV");
-// 			break ;
-// 		case D_QUOTE:
-// 			printf("%20s", "D_QUOTES");
-// 			break ;
-// 		case S_QUOTE:
-// 			printf("%20s", "S_QUOTES");
-// 			break ;
-// 		case WHITE_SPACE:
-// 			printf("%20s", "WHITE_SPACE");
-// 			break ;
-// 		case EMPTY:
-// 			printf("%20s", "EMPTY");
-// 			break ;
-// 		case EXIT_STATUS:
-// 			printf("%20s", "EXIT_STATUS");
-// 			break ;
-// 		default:
-// 			break ;
-// 		}
+void	print_tokens(t_elem *list)
+{
+	printf("%19s%10s%19s%14s\n", "CONTENT", "LEN", "TYPE", "STATE");
+	printf("%*s-----------------------------------------------------\n", 10,
+			" ");
+	while (list)
+	{
+		int padding = 20 - list->len;
+		printf("%*s::%s::", padding, " ", list->content);
+		printf("%9d", list->len);
+		switch (list->type)
+		{
+		case WORD:
+			printf("%20s", "WORD");
+			break ;
+		case REDIR_OUT:
+			printf("%20s", "REDIR_OUT");
+			break ;
+		case REDIR_IN:
+			printf("%20s", "REDIR_IN");
+			break ;
+		case DREDIR_OUT:
+			printf("%20s", "DREDIR_OUT");
+			break ;
+		case PIPE:
+			printf("%20s", "PIPE");
+			break ;
+		case HERE_DOC:
+			printf("%20s", "HERE_DOC");
+			break ;
+		case ENV:
+			printf("%20s", "ENV");
+			break ;
+		case D_QUOTE:
+			printf("%20s", "D_QUOTES");
+			break ;
+		case S_QUOTE:
+			printf("%20s", "S_QUOTES");
+			break ;
+		case WHITE_SPACE:
+			printf("%20s", "WHITE_SPACE");
+			break ;
+		case EMPTY:
+			printf("%20s", "EMPTY");
+			break ;
+		case EXIT_STATUS:
+			printf("%20s", "EXIT_STATUS");
+			break ;
+		default:
+			break ;
+		}
 
-// 		switch (list->state)
-// 		{
-// 		case IN_DQUOTE:
-// 			printf("%15s\n", "IN_DQUOTE");
-// 			break ;
-// 		case IN_QUOTE:
-// 			printf("%15s\n", "IN_QUOTE");
-// 			break ;
-// 		case GENERAL:
-// 			printf("%15s\n", "GENERAL");
-// 			break ;
-// 		default:
-// 			break ;
-// 		}
-// 		list = list->next;
-// 	}
-// 	printf("%*s-----------------------------------------------------\n", 10,
-// 			" ");
-// }
+		switch (list->state)
+		{
+		case IN_DQUOTE:
+			printf("%15s\n", "IN_DQUOTE");
+			break ;
+		case IN_QUOTE:
+			printf("%15s\n", "IN_QUOTE");
+			break ;
+		case GENERAL:
+			printf("%15s\n", "GENERAL");
+			break ;
+		default:
+			break ;
+		}
+		list = list->next;
+	}
+	printf("%*s-----------------------------------------------------\n", 10,
+			" ");
+}
